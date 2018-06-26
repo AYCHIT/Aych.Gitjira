3dc47ecc4c219c4ef61dbf3726bd312cba5943cb
Primarily Snark <starkey.joshua@gmail.com>
2018-06-26 13:29:47 -0700

    fixup! Fix client key encryption.

--- db/migrations/20180626202742-add-client-keys.js
+++ db/migrations/20180626202742-add-client-keys.js
@@ -3,7 +3,7 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.addColumn('Installations', 'clientKey', {
-      type: DataTypes.STRING,
+      type: Sequelize.STRING,
       allowNull: false
     })
   },
